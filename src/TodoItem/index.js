5e1a5ae81037b2d63da80b34168acd514050a472
ivanaMesterLightIt <ivana.mester@lightit.io>
2022-11-02 10:22:54 -0300

    add icon components

--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { CompleteIcon } from "../TodoIcon/CompleteIcon";
+import { DeleteIcon } from "../TodoIcon/DeleteIcon";
 import './TodoItem.css'
 
 function TodoItem(props){
@@ -9,21 +11,16 @@ function TodoItem(props){
 
     return (
         <li className="TodoItem">
-          <span 
-          onClick={props.onComplete}
-          className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
-          >
-            √
-          </span>
+          <CompleteIcon
+            completed={props.completed}
+            onComplete={props.onComplete}
+          />
           <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
             {props.text}
           </p>
-          <span
-          onClick={props.onDelete} 
-          className="Icon Icon-delete"
-          >
-            X
-          </span>
+          <DeleteIcon
+            onDelete={props.onDelete}
+          />
         </li>
       );
 }
